e9938ebb9d91ca6d2007f91b075613e0704701a7
U-DESKTOP-DHJ6T3E\Александр <Alexandr>
2019-04-23 19:26:36 +0300

    Added models of title and discriptions of recipes

--- web_cooking/recipes/models.py
+++ web_cooking/recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class add_recipe(models.Model):
+    title = models.CharField(max_length = 255, help_text ='Название рецепта:')
+    description = models.TextField(max_length = 255, help_text= 'Описание рецепта')
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank = True)
