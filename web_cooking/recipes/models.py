14d821c1eb3290908065764706c57b7d80a5d0b8
Nikita Zuev <Niki10222@bk.ru>
2019-04-24 18:56:47 +0300

    .gitignore was fullfilled and model of Category was added

--- web_cooking/recipes/models.py
+++ web_cooking/recipes/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length = 255 )
+
 
 class add_recipe(models.Model):
     title = models.CharField(max_length = 255, help_text ='Название рецепта:')
     description = models.TextField(max_length = 255, help_text= 'Описание рецепта')
     date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(blank = True)
+    category = models.ForeignKey(Category, null = True, blank= True,
+                                 on_delete=models.SET_NULL)
