76116a47e04e2a9d0ac3d397f11945acacce44bb
Zakharov Alexandr <Alexandr>
2019-04-25 20:58:06 +0300

    Fixed model of recipe

--- web_cooking/recipes/admin.py
+++ web_cooking/recipes/admin.py
@@ -3,4 +3,4 @@ from .models import *
 # Register your models here.
 
 
-admin.site.register(add_recipe)
+admin.site.register(Add_recipe)
--- web_cooking/recipes/models.py
+++ web_cooking/recipes/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class add_recipe(models.Model):
+class Add_recipe(models.Model):
     title = models.CharField(max_length = 255, help_text ='Название рецепта:')
-    description = models.TextField(max_length = 255, help_text= 'Описание рецепта')
-    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(help_text= 'Описание рецепта')
+    updated = models.DateTimeField(auto_now_add = False, auto_now = True)
+    date = models.DateTimeField(auto_now_add = True, auto_now = False)
     image = models.ImageField(blank = True)
