17321a1ebb4ef2d46059794e86b39bde07db0a85
Veremeva Nataliya <xopa144@yandex.ru>
2019-04-29 10:08:44 +0300

    added buttons

--- script_q1.js
+++ script_q1.js
@@ -4,24 +4,26 @@ $('.slider').each(function(){    // если слайдеров нескольк
     let $slides = $this.find('.slide');
     let currentIndex = 0;
     let timeout;
+    let backButton = $this.find('#backward');
+    let forButton = $this.find('#forward');
+    let flipSlide = '100%';  // для текущего слайда
+    let moveGroup = '-100%';  // для группы слайдов
 
     function moveSlides(index) {
-        let flipSlide = '100%';  // для текущего слайда
-        let moveGroup = '-100%';  // для группы слайдов
         advance();
 
-        if($group.is('.animated') || currentIndex === index){
+        if($group.is(':animated') || currentIndex === index){
             return;
         }
 
         $slides.eq(index).css({left: flipSlide, display: 'block'});
-        $group.animate({left: moveGroup}, function(){
+        $group.animate({left: moveGroup},600, function(){
             $slides.eq(currentIndex).css({display: 'none'});
             $slides.eq(index).css({left: 0});
             $group.css({left: 0});
             currentIndex = index;
         });
-    };
+    }
 
     function advance(){
         clearTimeout(timeout);
@@ -31,8 +33,28 @@ $('.slider').each(function(){    // если слайдеров нескольк
             }else{
                 moveSlides(0);
             }
-        }, 3000);
+        }, 2600);
     }
+
+    backButton.on('click',function(){
+        flipSlide = '-100%';
+        moveGroup = '100%';
+        if (currentIndex === 0){
+            moveSlides($slides.length - 1)
+        }else{
+            moveSlides(currentIndex - 1)
+        }
+        flipSlide = '100%';
+        moveGroup = '-100%';
+    });
+
+    forButton.on('click',function(){
+        if (currentIndex === $slides.length - 1){
+            moveSlides(0)
+        }else{
+            moveSlides(currentIndex + 1)
+        }
+    });
 
     advance();
 });
