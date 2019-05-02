02c9f9e8a9f93ba52693b448ddfb2ad107df71eb
Panafidin Egor <panafidin.egor@gmail.com>
2019-05-03 01:25:37 +0300

    Changing slider

--- js/script.js
+++ js/script.js
@@ -1,60 +1,12 @@
-$('.slider').each(function(){    // если слайдеров несколько
-    let $this = $(this);
-    let $group = $this.find('.slide-group');
-    let $slides = $this.find('.slide');
-    let currentIndex = 0;
-    let timeout;
-    let backButton = $this.find('#backward');
-    let forButton = $this.find('#forward');
-    let flipSlide = '100%';  // для текущего слайда
-    let moveGroup = '-100%';  // для группы слайдов
+﻿$('section.awSlider .carousel').carousel({
+	pause: "hover",
+  interval: 2000
+});
 
-    function moveSlides(index) {
-        advance();
+var startImage = $('section.awSlider .item.active > img').attr('src');
+$('section.awSlider').append('<img src="' + startImage + '">');
 
-        if($group.is(':animated') || currentIndex === index){
-            return;
-        }
-
-        $slides.eq(index).css({left: flipSlide, display: 'block'});
-        $group.animate({left: moveGroup},600, function(){
-            $slides.eq(currentIndex).css({display: 'none'});
-            $slides.eq(index).css({left: 0});
-            $group.css({left: 0});
-            currentIndex = index;
-        });
-    }
-
-    function advance(){
-        clearTimeout(timeout);
-        timeout = setTimeout(function(){
-            if (currentIndex < ($slides.length - 1)){
-                moveSlides(currentIndex + 1);
-            }else{
-                moveSlides(0);
-            }
-        }, 2600);
-    }
-
-    backButton.on('click',function(){
-        flipSlide = '-100%';
-        moveGroup = '100%';
-        if (currentIndex === 0){
-            moveSlides($slides.length - 1)
-        }else{
-            moveSlides(currentIndex - 1)
-        }
-        flipSlide = '100%';
-        moveGroup = '-100%';
-    });
-
-    forButton.on('click',function(){
-        if (currentIndex === $slides.length - 1){
-            moveSlides(0)
-        }else{
-            moveSlides(currentIndex + 1)
-        }
-    });
-
-    advance();
+$('section.awSlider .carousel').on('slid.bs.carousel', function () {
+ var bscn = $(this).find('.item.active > img').attr('src');
+	$('section.awSlider > img').attr('src',bscn);
 });
