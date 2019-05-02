f820288fc0164a5efc197d56486fa2762922feae
Panafidin Egor <panafidin.egor@gmail.com>
2019-05-03 02:07:06 +0300

    Fixed slider

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-﻿$('section.awSlider .carousel').carousel({
-	pause: "hover",
-  interval: 2000
-});
+﻿/* SLIDER MAIN PAGE */
 
-var startImage = $('section.awSlider .item.active > img').attr('src');
-$('section.awSlider').append('<img src="' + startImage + '">');
+$('section.awSlider .carousel').carousel({
+	pause: "hover",
+  interval: 4000
+});
 
 $('section.awSlider .carousel').on('slid.bs.carousel', function () {
  var bscn = $(this).find('.item.active > img').attr('src');
