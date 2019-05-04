10732b36a025832ca6372aa26a45a78580889cc9
Panafidin Egor <panafidin.egor@gmail.com>
2019-05-04 23:20:29 +0300

    Improving menu options

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@ $('section.awSlider .carousel').on('slid.bs.carousel', function () {
  var bscn = $(this).find('.item.active > img').attr('src');
 	$('section.awSlider > img').attr('src',bscn);
 });
+
+$(document).ready(function(){
+  $('a').on('click', function(e){
+    e.preventDefault();
+  });
+    
+  $('#ddmenu li').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('ul',this).stop(true,true).slideDown(200);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('ul',this).stop(true,true).slideUp(200);
+    }, this), 100));
+  });
+
+});
