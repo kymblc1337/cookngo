71a2e296f2a41622959170dcbc65616813bde1d6
Panafidin Egor <panafidin.egor@gmail.com>
2019-05-07 23:54:29 +0300

    Working ingredients menu in js file

--- js/script.js
+++ js/script.js
@@ -15,41 +15,61 @@ $('section.awSlider .carousel').on('slid.bs.carousel', function () {
 $(document).ready(function() {
 
   // Добавляем значения
-  var val_data = {
-    "val1": "Значение 1",
-    "val2": "Значение 2",
-    "val3": "Значение 3",
-    "val4": "Значение 4",
-    "val5": "Значение 5"
-  };
+  	var val_data = {
+	    "val1": "Значение 1",
+	    "val2": "Значение 2",
+	    "val3": "Значение 3",
+	    "val4": "Значение 4",
+	    "val5": "Значение 5"
+  	};
 
-  var input = $('[name="choose_cousine"]');
+  	var ingredients_menu = {
+	  	'[name="choose_cousine"]': "<a><span class='open'>Кухня</span><span class='value'></span></a>",
+	  	'[name="choose_menu"]': "<a><span class='open'>Кухня</span><span class='value'></span></a>",
+	  	'[name="choose_category"]': "<a><span class='open'>Кухня</span><span class='value'></span></a>",
+	  	'[name="choose_dish"]': "<a><span class='open'>Кухня</span><span class='value'></span></a>"
+	};
+
+	for (elem in ingredients_menu) {
+		var input = $('[name="choose_cousine"]');
   
-  // Создаем общий блок с классом
-  var val_cont = document.createElement('div');
-  $(val_cont).addClass("dropdown_cousine");
+	  	var val_cont = document.createElement('div');
+	  	$(val_cont).addClass("dropdown_cousine");
 
-  // Создаем кнопку открытия списка и поле для записи значений
-  $(val_cont).append("<a href='javascript:void(0);'><span class='open'>Кухня</span><span class='value'></span></a>");
+	  	$(val_cont).append("<a href='javascript:void(0);'><span class='open'>Кухня</span><span class='value'></span></a>");
 
-  // Создаем выпдающий список и вкладываем в общий блок
-  var ul = document.createElement('ul');
-  $(ul).addClass("choose_cousine_menu");
-  for (elem in val_data) {
-    $(ul).append("<li><input type='checkbox' value='" + elem + "' id='" + elem + "'><label for='" + elem + "'>" + val_data[elem] + "</label></li>");
-  }
-  $(ul).appendTo(val_cont);
-  $(ul).hide();
+	  	var ul = document.createElement('ul');
+	  	$(ul).addClass("choose_cousine_menu");
+	  	for (elem in val_data) {
+	    	$(ul).append("<li><input type='checkbox' value='" + elem + "' id='" + elem + "'><label for='" + elem + "'>" + val_data[elem] + "</label></li>");
+	  	}
+	  	$(ul).appendTo(val_cont);
 
-  // Размещаем общий блок после нужного input-а
-  $(input).after(val_cont);
+	  	$(input).after(val_cont);
+	}
 
-  // Скрываем/открываем выпадающий список
-  $(".dropdown_cousine a").on('click', function() {
-    $(".dropdown_cousine ul").slideToggle('fast');
-  });
+  	var input = $('[name="choose_cousine"]');
   
-  $('.dropdown_cousine ul input[type="checkbox"]').on('click', function() {
+  	var val_cont = document.createElement('div');
+  	$(val_cont).addClass("dropdown_cousine");
+
+  	$(val_cont).append("<a href='javascript:void(0);'><span class='open'>Кухня</span><span class='value'></span></a>");
+
+  	var ul = document.createElement('ul');
+  	$(ul).addClass("choose_cousine_menu");
+  	for (elem in val_data) {
+    	$(ul).append("<li><input type='checkbox' value='" + elem + "' id='" + elem + "'><label for='" + elem + "'>" + val_data[elem] + "</label></li>");
+  	}
+  	$(ul).appendTo(val_cont);
+  	$(ul).hide();
+
+  	$(input).after(val_cont);
+
+  	$(".dropdown_cousine a").on('click', function() {
+    	$(".dropdown_cousine ul").slideToggle('medium');
+  	});
+  
+  	$('.dropdown_cousine ul input[type="checkbox"]').on('click', function() {
 
     var title_val = $(this).closest('.dropdown_cousine ul').find('input[type="checkbox"]').val(),
       title = $(this).val() + ", ";
