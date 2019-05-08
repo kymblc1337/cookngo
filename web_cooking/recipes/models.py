f7c7ea1b214b4cd90ec47a205299d12a4a49aae9
Zakharov Alexandr <zaharovp@yandex.ru>
2019-05-08 20:44:42 +0300

    pics deleted

--- web_cooking/recipes/models.py
+++ web_cooking/recipes/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Add_recipe(models.Model):
-    title = models.CharField(max_length = 255, help_text ='Название рецепта:')
-    description = models.TextField(help_text= 'Описание рецепта')
-    updated = models.DateTimeField(auto_now_add = False, auto_now = True)
-    date = models.DateTimeField(auto_now_add = True, auto_now = False)
-    image = models.ImageField(blank = True)
-
+    title = models.CharField(max_length=255, help_text='Название рецепта:')
+    description = models.TextField(help_text='Описание рецепта')
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    date = models.DateTimeField(auto_now_add=True, auto_now=False)
+    image = models.ImageField(blank=True)
     def __unicode__(self):
         return self.title
 
