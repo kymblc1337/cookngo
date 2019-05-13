4b10ecaaade51d8cc635400b8d19c9bb368e93dc
Veremeva Nataliya <xopa144@yandex.ru>
2019-05-14 00:08:21 +0300

    minor changes

--- loadpages.js
+++ loadpages.js
@@ -10,27 +10,10 @@ xhr.send(null);
 $('nav a').on('click',function(e){
     e.preventDefault();
     let url=this.href;
-    var $content = $('#content');
 
     $('nav a.current').removeClass('current');
     $(this).addClass('current');
     $('#container').remove();
 
-    $.ajax({
-        type:"POST",
-        url:url,
-        timeout:2000,
-        beforeSend: function(){
-            $content.append('<div id="load">Loading</div>');
-        },
-        complete:function(){
-            $('#loading').remove();
-        },
-        success: function(data){
-            $content.html($(data).find('#container')).hide().fadeIn(400);
-        },
-        fail:function(){
-            $('#panel').html('<div class="loading">Please try again soon.</div>');
-        }
-    });
+    $('#content').load(url+' #content');
 });
