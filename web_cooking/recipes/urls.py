5892816da4e4e8857d067c7d7c9507adb579c072
Nikita <Nikita6500@gmail.com>
2019-05-27 17:35:48 +0300

    ulr fixed

--- web_cooking/recipes/urls.py
+++ web_cooking/recipes/urls.py
@@ -8,8 +8,9 @@ admin.autodiscover()
 urlpatterns = [
     path('', views.Index.as_view(), name ="home"),
     path('info/<int:id>', views.post_detail, name = 'detail'),
-    path('login', login),
+    path('accounts/login/', login),
     path('accounts/logout', logout),
-    path('accounts/reg', registration),
-    path('add/', Add_view.as_view())
+    path('accounts/registration/', registration),
+    path('add/', Add_view.as_view()),
+    path('userpage/', userpage),
 ]
